dc758fea11d014e4549db41bd02c5f84f92611f9
Your Name <you@example.com>
2024-11-06 22:47:43 -0800

    added 3rd requirement

---
+++ Scipt.js
@@ -0,0 +1,3 @@
+let numbers = [5, 10, 15, 20, 25];
+numbers.push(30, 35, 40, 45, 50);
+console.log(numbers);
--- script.js
+++ script.js
@@ -5,4 +5,10 @@ console.log(numbers[0]);
 console.log(numbers[numbers.length - 1]);
 
 
+let number = [5, 10, 15, 20, 25];
+numbers.push(30, 35, 40, 45, 50);
+console.log(numbers);
+
+
+
 
